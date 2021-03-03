16439a088b4c4b4d99763bab3861fdbafc829dfb
VaSeBa <badasin.valentin@yandex.ru>
2021-03-03 13:23:40 +0300

    commit

--- src/main/java/ru/vaseba/sarafun/domain/User.java
+++ src/main/java/ru/vaseba/sarafun/domain/User.java
@@ -1,9 +1,6 @@
 package ru.vaseba.sarafun.domain;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
