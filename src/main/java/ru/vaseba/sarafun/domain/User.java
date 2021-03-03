f182452a47cd037a44496601ec22063b9d26ad58
VaSeBa <badasin.valentin@yandex.ru>
2021-03-03 08:39:47 +0300

    new commit

--- src/main/java/ru/vaseba/sarafun/domain/User.java
+++ src/main/java/ru/vaseba/sarafun/domain/User.java
@@ -7,14 +7,13 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "usr")
 @Data
-@Getter
-@Setter
-public class User {
+public class User implements Serializable {
     @Id
     private String id;
     private String name;
